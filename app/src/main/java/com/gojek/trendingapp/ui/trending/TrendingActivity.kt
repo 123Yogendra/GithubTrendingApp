c7040fba0e218132a8b3d62a715fe002f2b373ab
datanapps <datanapps@gmail.com>
2020-01-19 04:05:04 +0800

    added menu and toolbar

--- app/src/main/java/com/gojek/trendingapp/ui/trending/TrendingActivity.kt
+++ app/src/main/java/com/gojek/trendingapp/ui/trending/TrendingActivity.kt
@@ -1,15 +1,26 @@
 package com.gojek.trendingapp.ui.trending
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.gojek.trendingapp.R
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.layout_toolbar.*
+import kotlinx.android.synthetic.main.trending_activity.*
+
 
 class TrendingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.trending_activity)
+
+        // update toolbar
+        setSupportActionBar(toolbar)
+
+        // set fragment
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, TrendingFregment.newInstance())
@@ -20,7 +31,17 @@ class TrendingActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_trending, menu)
-        return true
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.getItemId()) {
+            R.id.action -> {
+                Snackbar.make(activity_trending_root, getString(R.string.msg_action), Snackbar.LENGTH_INDEFINITE).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
